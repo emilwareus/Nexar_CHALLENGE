b2dd31c0b7502584f71208e9ad75458bbfc46fd1
NikeNano <niklas.sven.hansson@gmail.com>
2017-09-23 09:34:44 +0200

    added script for setup of everything

--- setUpModels.sh
+++ setUpModels.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 echo "This is a test script!"
 git clone https://github.com/tensorflow/models.git
-apt-get install protobuf-compiler python-pil python-lxml
-pip install jupyter
-pip install matplotlib
+yes Y |sudo apt-get install protobuf-compiler python-pil python-lxml
+sudo -H pip install jupyter
+sudo -H pip install matplotlib
 ls
-cd models
+cd models/research
 protoc object_detection/protos/*.proto --python_out=.
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 sudo python3 setup.py install 
 echo "The installation script is now done"
+cd object_detection
+#mkdir data
+mkdir images
+cd images
+mkdir test
+mkdir train
+cd ..
+mkdir training
+echo "Now you need to add the data to the different folders"
+cd ..
+python object_detection/builders/model_builder_test.py
+echo "If the test passed the instalation was succesfull"
+
+
